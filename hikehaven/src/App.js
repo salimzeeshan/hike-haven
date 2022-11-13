8948c38222df514378c3734af9103c61831d3d28
salimzeeshan <zeeshasalim541@gmail.com>
2022-11-13 19:57:00 +0530

    day-5

--- hikehaven/src/App.js
+++ hikehaven/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/Navbar"
+import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
     <>
       <Navbar />
+      <AllRoutes />
     </>
   )
 }
